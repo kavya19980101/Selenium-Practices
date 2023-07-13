2571ce8a42b25b0277292ef4609f657605683d2d
kavya19980101 <kavyastestings@gmail.com>
2023-07-14 00:43:25 +0530

    Headless mode understanding2

--- src/test/java/org/example/Headless_mode_Demo.java
+++ src/test/java/org/example/Headless_mode_Demo.java
@@ -6,20 +6,13 @@ import org.testng.annotations.Test;
 
 public class Headless_mode_Demo {
 
-       ChromeDriver driver=new ChromeDriver();
-
     @Test
-    public void Headless(){
-        //ChromeOptions options=new ChromeOptions();
-        //options.setHeadless(true);
-        //ChromeDriver driver=new ChromeDriver();
+    public static void Headless(){
+        ChromeOptions options=new ChromeOptions();
+        options.setHeadless(true);
+        ChromeDriver driver=new ChromeDriver(options);
         driver.get("https://app.vwo.com");
         String title=driver.getTitle();
         System.out.println(title);
-    }
-    @Test
-    public void Headless1(){
-        String getCurrentUrl=driver.getCurrentUrl();
-        System.out.println(getCurrentUrl);
     }
 }
