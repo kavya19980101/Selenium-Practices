ac63363b870b82c7eddc1ca29e07b271aa8d0049
kavya19980101 <kavyastestings@gmail.com>
2023-07-14 16:45:17 +0530

    Login page automated

--- src/test/java/org/example/Pageloadstrategy.java
+++ src/test/java/org/example/Pageloadstrategy.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,12 +13,17 @@ public class Pageloadstrategy {
     {
         ChromeOptions options=new ChromeOptions();
         options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.addArguments("start-maximized");
         WebDriver driver=new ChromeDriver(options);
         driver.get("https://app.vwo.com");
-        driver.close();//Will close the current window and session remain same..will get ->invalid session id
-        driver.quit();//Will close all the windows and session=null..will get-> session is nulll error
+        //driver.close();//Will close the current window and session remain same..will get ->invalid session id
+        //driver.quit();//Will close all the windows and session=null..will get-> session is nulll error
         String title=driver.getTitle();
        System.out.println(title);
-
+       driver.findElement(By.xpath("//*[@id=\"login-username\"]")).sendKeys("kavyashekarvijaya@gmail.com");
+        driver.findElement(By.xpath("//*[@id=\"login-password\"]")).sendKeys("Hari@20010110");
+        driver.findElement(By.xpath("//*[@id=\"js-login-btn\"]")).click();
+       String url=driver.getCurrentUrl();
+        System.out.println("The URL of the Dashboard "+url);
     }
 }
